8cf7960aede89f66fc49cfbbbe43342f11104c69
Slyther <joseph.a.griff_07@hotmail.com>
2015-08-04 20:26:00 -0600

    Updated seeders

--- Mhotivo/Migrations/Configuration.cs
+++ Mhotivo/Migrations/Configuration.cs
@@ -1,27 +1,25 @@
 using Mhotivo.Data.Entities;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Mhotivo.Implement.Context;
 
 namespace Mhotivo.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<Mhotivo.Implement.Context.MhotivoContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<MhotivoContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Mhotivo.Implement.Context.MhotivoContext context)
+        protected override void Seed(MhotivoContext context)
         {
             context.Roles.AddOrUpdate(new Role { Id = 1, Description = "Admin", Name = "Admin" });
             context.Roles.AddOrUpdate(new Role { Id = 2, Description = "Padres", Name = "Padres" });
             context.Roles.AddOrUpdate(new Role { Id = 3, Description = "Maestro", Name = "Maestro" });
             context.SaveChanges();
-            context.Users.AddOrUpdate(new User { Id = 1, DisplayName = "La directora", Email = "holis@holis.com", Password = "holis", Status = true });
-            context.Users.AddOrUpdate(new User { Id = 2, DisplayName = "Default", Email = "default@holis.com", Password = "123456", Status = true });
+            context.Users.AddOrUpdate(new User { Id = 1, DisplayName = "Administrador", Email = "admin@mhotivo.edu", Password = "password", Status = true });
+            context.Users.AddOrUpdate(new User { Id = 2, DisplayName = "Maestro Generico", Email = "teacher@mhotivo.edu", Password = "password", Status = true });
             context.SaveChanges();
             var rol1 = context.Roles.First();
             var rol2 = context.Roles.Find(3);
@@ -30,10 +28,10 @@ namespace Mhotivo.Migrations
             context.UserRoles.AddOrUpdate(new UserRol { Id = 1, Role = rol1, User = user1 });
             context.UserRoles.AddOrUpdate(new UserRol { Id = 2, Role = rol2, User = user2 });
             context.SaveChanges();
-            var maestroDefault = context.Meisters.FirstOrDefault(x => x.FirstName == "Default");
+            var maestroDefault = context.Meisters.FirstOrDefault(x => x.FirstName == "Maestro Generico");
             if (maestroDefault == null)
             {
-                context.Meisters.AddOrUpdate(new Teacher { Id = 1, IdNumber = "0000000000000", FirstName = "Default", LastName = "Default", FullName = "Default Default", Disable = false, Gender = true, User = user2 });
+                context.Meisters.AddOrUpdate(new Teacher { Id = 1, IdNumber = "0000000000000", FirstName = "Maestro", LastName = "Generico", FullName = "Maestro Generico", Disable = false, Gender = true, User = user2 });
                 context.SaveChanges();
             }
             context.NotificationTypes.AddOrUpdate(new NotificationType { NotificationTypeId = 1, TypeDescription = "General" });
