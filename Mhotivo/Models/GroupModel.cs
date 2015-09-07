aed536a84e5bf7382573f7ec587213496e644936
Slyther <joseph.a.griff_07@hotmail.com>
2015-09-07 14:49:06 -0600

    Appharbor changes 8

--- Mhotivo/Models/GroupModel.cs
+++ Mhotivo/Models/GroupModel.cs
@@ -5,6 +5,7 @@ namespace Mhotivo.Models
 {
     public class AddGroup
     {
+        public long Id { get; set; }
         [Required]
         [DisplayName("Name")]
         public string Name { get; set; }
