66de13551ab9f6f4f6bfd256eb2be6a82c81191b
Slyther <joseph.a.griff_07@hotmail.com>
2015-09-08 00:04:56 -0600

    Run migrations on Mhotivo Application Start

--- Mhotivo/Global.asax.cs
+++ Mhotivo/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Http;
+﻿using System.Data.Entity;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Mhotivo.Implement.Context;
+using Mhotivo.Migrations;
 
 namespace Mhotivo
 {
@@ -10,14 +13,13 @@ namespace Mhotivo
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<MhotivoContext>());
-            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<MhotivoContext, Configuration>());
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
             AutoMapperConfiguration.Configure();
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MhotivoContext, Configuration>());
         }
     }
 }
