d184c2ed43fba63c4d7faae6bc302453cc6a570c
Slyther <joseph.a.griff_07@hotmail.com>
2015-09-08 00:45:26 -0600

    Added tracer for appharbor debugging

--- Mhotivo/Controllers/AccountController.cs
+++ Mhotivo/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
 using System.Web.Mvc;
 using Mhotivo.Interface.Interfaces;
 using Mhotivo.Models;
@@ -32,19 +33,26 @@ namespace Mhotivo.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginModel model, string returnUrl)
         {
+            var traceSource = new TraceSource("AppHarborTraceSource", SourceLevels.All);
+            traceSource.TraceEvent(TraceEventType.Verbose, 0, "Got into Login method");
             var parent = _parentRepository.Filter(y => y.MyUser.Email == model.UserEmail).FirstOrDefault();
+            traceSource.TraceEvent(TraceEventType.Verbose, 0, "filtered parents");
             if (parent == null)
             {
+                traceSource.TraceEvent(TraceEventType.Verbose, 0, "not a parent");
                 if (_sessionManagement.LogIn(model.UserEmail, model.Password, model.RememberMe))
                 {
+                    traceSource.TraceEvent(TraceEventType.Verbose, 0, "logged in");
                     return RedirectToLocal(returnUrl);
                 }
             }
             else
             {
+                traceSource.TraceEvent(TraceEventType.Verbose, 0, "was a parent");
                 ModelState.AddModelError("", "El usurio no tiene privilegios para entrar a esta pagina");
                 return View(model);
             }
+            traceSource.TraceEvent(TraceEventType.Verbose, 0, "insufficient privileges");
             // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
             ModelState.AddModelError("", "El nombre de usuario o la contraseña especificados son incorrectos.");
             return View(model);
