f9c2b09ca0e3cd84e2243b54cc11bea78b7a9895
Miguel Chicas <immac.gm@gmail.com>
2015-11-17 01:30:21 -0600

    create AcademicGradeController and Views

---
+++ Mhotivo/Controllers/AcademicGradeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mhotivo.Controllers
+{
+    public class AcademicGradeController : Controller
+    {
+        //
+        // GET: /AcademicGrade/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
