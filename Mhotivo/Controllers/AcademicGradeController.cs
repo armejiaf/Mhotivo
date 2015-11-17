59d00960567bfa09cee6421024c3bb947102871a
Miguel Chicas <immac.gm@gmail.com>
2015-11-17 02:08:03 -0600

    index academic yeara additrons

--- Mhotivo/Controllers/AcademicGradeController.cs
+++ Mhotivo/Controllers/AcademicGradeController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using Mhotivo.Data.Entities;
+using Mhotivo.Interface.Interfaces;
+using Mhotivo.Models;
 
 namespace Mhotivo.Controllers
 {
     public class AcademicGradeController : Controller
     {
-        //
-        // GET: /AcademicGrade/
+        private readonly IAcademicGradeRepository _academicGradeRepository;
+        
 
-        public ActionResult Index()
+        public ActionResult Index(long yearId)
         {
-            return View();
+            var model = _academicGradeRepository.Filter(x => x.AcademicYear.Id == yearId).Select(Mapper.Map<AcademicGradeDisplayModel>);
+            return View(model);
         }
 
     }
