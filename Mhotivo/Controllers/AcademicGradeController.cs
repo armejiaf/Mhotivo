9b80932b5ee4cdf2a425e8fabfea1c7bd018a61b
Slyther <joseph.a.griff_07@hotmail.com>
2015-11-17 13:26:49 -0600

    Fixed AcademicGrade Index, AcademicYear Add, changed connection strings to their defaults, merged branch FixUser.

--- Mhotivo/Controllers/AcademicGradeController.cs
+++ Mhotivo/Controllers/AcademicGradeController.cs
@@ -6,19 +6,51 @@ using System.Web.Mvc;
 using AutoMapper;
 using Mhotivo.Data.Entities;
 using Mhotivo.Interface.Interfaces;
+using Mhotivo.Logic.ViewMessage;
 using Mhotivo.Models;
+using PagedList;
 
 namespace Mhotivo.Controllers
 {
     public class AcademicGradeController : Controller
     {
         private readonly IAcademicGradeRepository _academicGradeRepository;
-        
+        private readonly ViewMessageLogic _viewMessageLogic;
+        private readonly IAcademicYearRepository _academicYearRepository;
 
-        public ActionResult Index(long yearId)
+        public AcademicGradeController(IAcademicGradeRepository academicGradeRepository, IAcademicYearRepository academicYearRepository)
         {
-            var model = _academicGradeRepository.Filter(x => x.AcademicYear.Id == yearId).Select(Mapper.Map<AcademicGradeDisplayModel>);
-            return View(model);
+            _academicGradeRepository = academicGradeRepository;
+            _academicYearRepository = academicYearRepository;
+            _viewMessageLogic = new ViewMessageLogic(this);
+        }
+
+        public ActionResult Index(long yearId, string currentFilter, string searchString, int? page)
+        {
+            _viewMessageLogic.SetViewMessageIfExist();
+            var grades = _academicGradeRepository.Filter(x => x.AcademicYear.Id == yearId).ToList();
+            ViewBag.IdAcademicYear = yearId;
+            ViewBag.Year = _academicYearRepository.GetById(yearId).Year;
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                try
+                {
+                    grades = _academicGradeRepository.Filter(x => x.AcademicYear.Id == yearId && (x.Section.Equals(searchString) || x.Grade.Name.Contains(searchString))).ToList();
+                }
+                catch (Exception)
+                {
+                    grades = _academicGradeRepository.Filter(x => x.AcademicYear.Id == yearId).ToList();
+                }
+            }
+            ViewBag.CurrentFilter = searchString;
+            var model = grades.Select(Mapper.Map<AcademicGradeDisplayModel>);
+            const int pageSize = 10;
+            var pageNumber = (page ?? 1);
+            return View(model.ToPagedList(pageNumber, pageSize));
         }
 
     }
--- Mhotivo/Models/AcademicGradeModel.cs
+++ Mhotivo/Models/AcademicGradeModel.cs
@@ -1,28 +1,53 @@
-﻿namespace Mhotivo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Office.Interop.Excel;
+
+namespace Mhotivo.Models
 {
     public class AcademicGradeDisplayModel
     {
         public long Id { get; set; }
+
+        [Display(Name="Grado")]
         public string Grade { get; set; }
+
+        [Display(Name = "Seccion")]
         public string Section { get; set; }
+
+        [Display(Name = "Pensum Elegido")]
         public string ActivePensum { get; set; }
+
+        [Display(Name = "Maestro de Grado")]
         public string SectionTeacher { get; set; }
     }
 
     public class AcademicGradeRegisterModel
     {
         public long AcademicYear { get; set; }
+
+        [Display(Name = "Grado")]
         public long Grade { get; set; }
+
+        [Display(Name = "Seccion")]
         public string Section { get; set; }
+
+        [Display(Name = "Pensum a usarse")]
         public long ActivePensum { get; set; }
     }
 
     public class AcademicGradeEditModel
     {
         public long Id { get; set; }
+
+        [Display(Name = "Grado")]
         public long Grade { get; set; }
+
+        [Display(Name = "Seccion")]
         public string Section { get; set; }
+
+        [Display(Name = "Pensum a usarse")]
         public long ActivePensum { get; set; }
+
+        [Display(Name = "Maestro de Grado")]
         public long SectionTeacher { get; set; }
     }
 }
