ccbf364ec18386caeece6b93ff91501419250c1b
Slyther <joseph.a.griff_07@hotmail.com>
2015-11-25 15:58:09 -0600

    More automapper fixes, fixed notification not being updated to sent.

--- Mhotivo.ParentSite/App_Start/AutoMapperConfiguration.cs
+++ Mhotivo.ParentSite/App_Start/AutoMapperConfiguration.cs
@@ -9,7 +9,8 @@ namespace Mhotivo.ParentSite
         public static void Configure()
         {
             Mapper.CreateMap<Notification, NotificationModel>()
-                .ForMember(p => p.NotificationCreator, o => o.MapFrom(src => src.NotificationCreator.FullName));
+                .ForMember(p => p.NotificationCreator, o => o.MapFrom(src => src.NotificationCreator.FullName))
+                .ForMember(p => p.CommentsAmount, o => o.MapFrom(src => src.NotificationComments.Count));
             Mapper.CreateMap<NotificationComment, NotificationCommentsModel>().ReverseMap();
             Mapper.CreateMap<Homework, HomeworkModel>()
                 .ForMember(p => p.AcademicCourse, o => o.MapFrom(src => src.AcademicCourse.Course.Name));
--- Mhotivo.ParentSite/Models/NotificationModel.cs
+++ Mhotivo.ParentSite/Models/NotificationModel.cs
@@ -8,7 +8,7 @@ namespace Mhotivo.ParentSite.Models
     public class NotificationModel
     {
         public long Id { get; set; }
-        public string NotificationName { get; set; }
+        public string Title { get; set; }
         public string NotificationCreator { get; set; }
         public string Message { get; set; }
         public DateTime Created { get; set; }
