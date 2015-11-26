87a451dd25f02e47f80858554c7e69f26f95912c
jmibarrad <jmibarrad@gmail.com>
2015-11-26 09:04:26 -0600

    Teacher crud Model fixed

--- Mhotivo/Models/TeacherModel.cs
+++ Mhotivo/Models/TeacherModel.cs
@@ -66,6 +66,7 @@ namespace Mhotivo.Models
 
         [Required(ErrorMessage = "Debe Ingresar Fecha de Nacimiento")]
         [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "Debe Ingresar Ciudad")]
@@ -105,6 +106,7 @@ namespace Mhotivo.Models
 
         [Required(ErrorMessage = "Debe Ingresar Fecha de Nacimiento")]
         [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "Debe Ingresar Ciudad")]
