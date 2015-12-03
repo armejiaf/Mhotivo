3ee4d84ffeb50bf1ba0a7377911cc5e6766656b3
Miguel Chicas <immac.gm@gmail.com>
2015-12-02 23:14:46 -0600

    adds a parametrized authorization attribute

---
+++ Mhotivo/Authorizations/AuthorizeWithParameter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mhotivo.Interface.Interfaces;
+using Mhotivo.Util;
+
+namespace Mhotivo.Authorizations
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeWithParameter : AuthorizeAttribute
+    {
+        private readonly List<string> _requireAtLeastOnePrivileges;
+
+        public AuthorizeWithParameter(string privilegeString)
+        {
+            var privileges = privilegeString.Split(',');
+            this._requireAtLeastOnePrivileges = privileges.ToList();
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var roleRepository =
+                ((IRoleRepository) DependencyResolver.Current.GetService(typeof (IRoleRepository)));
+
+            var roleName = (string)HttpContext.Current.Session["loggedUserRole"];
+            var role = roleRepository.Filter(r => r.Name == roleName).FirstOrDefault(r => true);
+
+            return role != null && role.HasAnyPrivilege(_requireAtLeastOnePrivileges);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext context)
+        {
+            var urlHelper = new UrlHelper(context.RequestContext);
+            context.Result = new RedirectResult(urlHelper.Action("Index","Home"));
+        }
+    }
+}
