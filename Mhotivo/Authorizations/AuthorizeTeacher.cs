6b70de530b1d0b5f24f77fe262466cad61e7c566
Allan Mejia <mejia561@hotmail.com>
2015-12-05 15:24:02 -0600

    AdminSiteRedirect

--- Mhotivo/Authorizations/AuthorizeTeacher.cs
+++ Mhotivo/Authorizations/AuthorizeTeacher.cs
@@ -17,17 +17,19 @@ namespace Mhotivo.Authorizations
         {
             var roleRepository =
                 ((IRoleRepository) DependencyResolver.Current.GetService(typeof (IRoleRepository)));
-
+            var sessionManagementService = ((ISessionManagementService)DependencyResolver.Current.GetService(typeof(ISessionManagementService)));
             var roleName = (string)HttpContext.Current.Session["loggedUserRole"];
             var role = roleRepository.Filter(r => r.Name == roleName).FirstOrDefault(r => true);
-
-            return role != null && role.HasAnyPrivilege(RequireAtLeastOnePrivileges);
+            var user = ((IUserRepository)DependencyResolver.Current.GetService(typeof(IUserRepository))).GetById(Convert.ToInt64(sessionManagementService.GetUserLoggedId()));
+            return (!user.IsUsingDefaultPassword) && role != null && role.HasAnyPrivilege(RequireAtLeastOnePrivileges);
         }
 
         protected override void HandleUnauthorizedRequest(AuthorizationContext context)
         {
+            var sessionManagementService = ((ISessionManagementService)DependencyResolver.Current.GetService(typeof(ISessionManagementService)));
+            var user = ((IUserRepository)DependencyResolver.Current.GetService(typeof(IUserRepository))).GetById(Convert.ToInt64(sessionManagementService.GetUserLoggedId()));
             var urlHelper = new UrlHelper(context.RequestContext);
-            context.Result = new RedirectResult(urlHelper.Action("Index","Home"));
+            context.Result = user.IsUsingDefaultPassword ? new RedirectResult(urlHelper.Action("ChangePassword", "Account")) : new RedirectResult(urlHelper.Action("Index", "Home"));
         }
     }
 }
