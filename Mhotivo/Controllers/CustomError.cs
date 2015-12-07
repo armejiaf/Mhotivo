c7357e60f85b2550581ae890d587546a58df5449
Miguel Chicas <immac.gm@gmail.com>
2015-12-06 18:29:50 -0600

    Custom errors

---
+++ Mhotivo/Controllers/CustomError.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Mhotivo.Controllers
+{
+    public class CustomError : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
