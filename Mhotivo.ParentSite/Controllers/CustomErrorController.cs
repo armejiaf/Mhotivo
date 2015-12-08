383b28a19442b1607bc244652e128a7fc0619a0f
Slyther <joseph.a.griff_07@hotmail.com>
2015-12-07 18:25:37 -0600

    Included custom errors in parent site, updated outdated HTML references.

---
+++ Mhotivo.ParentSite/Controllers/CustomErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Mhotivo.ParentSite.Controllers
+{
+    public class CustomErrorController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
